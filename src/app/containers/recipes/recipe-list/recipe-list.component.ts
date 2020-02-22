aa5349f08c703302f8d9c2d461e3052f8fef546c
justinlazaro-iselect <justin.lazaro@iselect.com.au>
2020-02-23 06:51:11 +0800

    .

--- src/app/containers/recipes/recipe-list/recipe-list.component.ts
+++ src/app/containers/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from "../recipe.model";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
       "A Test Recipe",
@@ -14,8 +16,8 @@ export class RecipeListComponent implements OnInit {
       "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"
     ),
     new Recipe(
-      "A Test Recipe",
-      "This is a test maybe",
+      "Another Test Recipe",
+      "This is a test another",
       "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"
     )
   ];
@@ -23,4 +25,8 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
