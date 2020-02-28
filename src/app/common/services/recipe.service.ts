fb1cf6544d241c8eff4c597d3337850e3be9a568
justinlazaro-iselect <justin.lazaro@iselect.com.au>
2020-02-28 08:06:31 +0800

    .

--- src/app/common/services/recipe.service.ts
+++ src/app/common/services/recipe.service.ts
@@ -1,23 +1,34 @@
 import { Recipe } from "../models/recipe.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../models/ingredient.model";
+import { ShoppingListService } from "./shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      "A Test Recipe",
+      "Tasty Schnitzel",
       "This is a test maybe",
-      "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"
+      "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg",
+      [new Ingredient("Meat", 1), new Ingredient("French fries", 20)]
     ),
     new Recipe(
-      "Another Test Recipe",
+      "Big Fat Burger",
       "This is a test another",
-      "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg"
+      "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg",
+      [new Ingredient("Buns", 2), new Ingredient("Meat", 20)]
     )
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
--- src/app/containers/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/containers/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Recipe } from "../../../common/models/recipe.model";
+import { RecipeService } from "src/app/common/services/recipe.service";
 
 @Component({
   selector: "app-recipe-detail",
@@ -9,7 +10,11 @@ import { Recipe } from "../../../common/models/recipe.model";
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() {}
+  constructor(private recipeSerivce: RecipeService) {}
 
   ngOnInit() {}
+
+  onAddToShoppingList() {
+    this.recipeSerivce.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
