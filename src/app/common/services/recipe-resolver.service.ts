b2a8cbbd723d8153713892b53628a5d620dedb72
justinlazaro-iselect <justin.lazaro@iselect.com.au>
2020-03-04 07:34:31 +0800

    .

--- src/app/common/services/recipe-resolver.service.ts
+++ src/app/common/services/recipe-resolver.service.ts
@@ -6,12 +6,21 @@ import {
 } from "@angular/router";
 import { Recipe } from "../models/recipe.model";
 import { DataStorageService } from "../services/data-storage.service";
+import { RecipeService } from "./recipe.service";
 
 @Injectable({ providedIn: "root" })
 export class RecipeResolveService implements Resolve<Recipe[]> {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService
+  ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.dataStorageService.fetchRecipes();
+    const recipes = this.recipeService.getRecipes();
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
+    }
   }
 }
